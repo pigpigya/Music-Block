42a88a4190da10acdf0f567a5d5b8b8ea135f2dd
cower <28218043a@gmail.com>
2023-03-17 16:31:39 +0800

    change state to props

--- src/index.js
+++ src/index.js
@@ -30,13 +30,6 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null),
-			xIsNext: true,
-		};
-	}
 	handleClick(i) {
 		const squares = this.state.squares.slice();
 		if (calculateWinner(squares) || squares[i]) {
@@ -52,8 +45,8 @@ class Board extends React.Component {
 	renderSquare(i) {
 		return (
 			<Square
-				value={this.state.squares[i]}
-				onClick={() => this.handleClick(i)}
+				value={this.props.squares[i]}
+				onClick={() => this.props.onClick(i)}
 			/>
 		);
 	}
@@ -94,11 +87,13 @@ class Game extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			history: [{
-				squares: Array(9).fill(null),
-			}],
+			history: [
+				{
+					squares: Array(9).fill(null),
+				},
+			],
 			xIsNext: true,
-		}
+		};
 	}
 	render() {
 		return (
